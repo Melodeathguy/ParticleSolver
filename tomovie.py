2af11454c6eed3a127bcce79226624c19d9a7b17
fstah001 <fabian.stahl1@web.de>
2020-07-07 14:23:08 +0200

    some tomivie changes in script

--- tomovie.py
+++ tomovie.py
@@ -2,17 +2,21 @@ import sys
 import cv2
 import os
 
-resolution = (1680, 1050)
+resolution = (1280, 720)
+fps = 90.
 
 def main(root):
     
     fourcc = cv2.VideoWriter_fourcc(*'XVID')
-    writer = cv2.VideoWriter('output.avi', fourcc, 100.0, resolution)
+    writer = cv2.VideoWriter('output.avi', fourcc, fps, resolution)
     
     images = sorted([os.path.join(root, img) for img in os.listdir(root)])
     
     for i_path in images:
         img = cv2.imread(i_path)
+        
+        img = cv2.resize(img, resolution)
+        
         writer.write(img)
         
         cv2.imshow("pp", img)
